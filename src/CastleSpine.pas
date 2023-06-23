479a4a222adcf08755acdefc28aa2f1a689c4ebf
Kagamma <kagamma.km@gmail.com>
2023-06-23 13:22:20 +0700

    Update license

--- src/CastleSpine.pas
+++ src/CastleSpine.pas
@@ -1,5 +1,5 @@
 {
-  Copyright (c) 2022-2022 Trung Le (Kagamma).
+  Copyright (c) 2022-2023 kagamma.
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
